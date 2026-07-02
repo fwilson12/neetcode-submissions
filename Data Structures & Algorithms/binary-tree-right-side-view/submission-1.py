b6e1342a4103afe9e284dfee5b66e6af3a85fc41
Fletcher Wilson <145033473+fwilson12@users.noreply.github.com>
2026-07-02 18:42:14 -0500

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+ 
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        
+        # bfs, but only append the last node popped 
+
+        if root is None: return []
+
+        res = []
+        q = deque([root])
+
+        while q:
+            
+            # collect all nodes from this level
+            levelNodes = []
+            while q: 
+                levelNodes.append(q.popleft())
+
+            # add children to q in order
+            for node in levelNodes:
+                if node.left: q.append(node.left)
+                if node.right: q.append(node.right)
+            
+            # just want the value of the right-most node on this level
+            res.append(levelNodes[-1].val)
+
+        return res
